cf7983e12b058c7919b4a8f7a03799fb318081a6
Gastón Pappaterra <gpappaterra@hotmail.com>
2022-11-19 09:16:43 +0000

    Comence a configurar los tunks con la accion de crear una nueva nota usando firestore

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,30 +1,36 @@
 import { AddOutlined } from "@mui/icons-material";
 import { IconButton } from "@mui/material";
+import { useDispatch } from "react-redux";
+import { startNewNote } from "../../store/journal";
 import { JournalLayout } from "../layout/JournalLayout";
 import { NoteView, NothingSelectedView } from "../views";
 
 export const JournalPage = () => {
+  const dispatch = useDispatch();
+
+  const onClickNewNote = () => {
+    dispatch(startNewNote());
+  };
+
   return (
     <JournalLayout>
-
       {/* <NothingSelectedView/> */}
-      <NoteView/>
-
+      <NoteView />
 
       <IconButton
-        size='large'
+        onClick={onClickNewNote}
+        size="large"
         sx={{
-          color: 'white',
-          backgroundColor: 'error.main',
-          ':hover' : { backgroundColor: 'error.main', opacity: 0.9 },
-          position: 'fixed',
+          color: "white",
+          backgroundColor: "error.main",
+          ":hover": { backgroundColor: "error.main", opacity: 0.9 },
+          position: "fixed",
           right: 50,
-          bottom: 50
+          bottom: 50,
         }}
       >
         <AddOutlined sx={{ fontSize: 30 }} />
       </IconButton>
-
     </JournalLayout>
   );
 };
